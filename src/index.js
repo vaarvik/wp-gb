facebbf74fe7ec6410f641db767e81eb0f715fde
vaarvik <johannvaarvik@hotmail.com>
2022-09-25 19:11:44 +0200

    Change to older version of React to make @wordpress/components work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './assets/styles/style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM;
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

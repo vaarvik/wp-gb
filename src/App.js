d816dfc90996533cf2ca6a45cf25be2c355f614b
vaarvik <johannvaarvik@hotmail.com>
2022-09-25 19:13:14 +0200

    Update basics of styles and App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <main className="site-content">
+        <div className="block-content wrapper wide" id="block-content">
+
+        </div>
       </main>
     </div>
   );

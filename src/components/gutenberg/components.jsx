0c60588a1085d94b7267909629b2be127798dedc
johann.varvik@drift.oslo.kommune.no <johann.varvik@drift.oslo.kommune.no>
2022-09-27 23:09:34 +0200

    Add WPAnglePickerControl

--- src/components/gutenberg/components.jsx
+++ src/components/gutenberg/components.jsx
@@ -1,4 +1,5 @@
 import WPAlignmentMatrixControl from "./components/WPAlignmentMatrixControl";
+import WPAnglePickerControl from "./components/WPAnglePickerControl";
 import WPButton from "./components/WPButton";
 
 const components = [
@@ -11,6 +12,11 @@ const components = [
         title: "Alignment Matrix Control",
         summary: "Alignment for background images for example.",
         render: WPAlignmentMatrixControl
+    },
+    {
+        title: "Angle Picker Control",
+        summary: "When in need of an angle.",
+        render: WPAnglePickerControl
     },
 ];
 
---
+++ src/components/gutenberg/components/WPAnglePickerControl.jsx
@@ -0,0 +1,12 @@
+import { AnglePickerControl } from "@wordpress/components";
+
+
+function WPAnglePickerControl(){
+    return (
+        <div className="wp-component-wrapper bg">
+            <AnglePickerControl />
+        </div>
+    )
+}
+
+export default WPAnglePickerControl;

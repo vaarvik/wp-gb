9973fb4b7490d2575932485b5becabe131bca0cb
johann.varvik@drift.oslo.kommune.no <johann.varvik@drift.oslo.kommune.no>
2022-09-27 23:33:46 +0200

    Fix bug in AnglePickerControl with onChange

--- src/components/gutenberg/components/WPAnglePickerControl.jsx
+++ src/components/gutenberg/components/WPAnglePickerControl.jsx
@@ -1,10 +1,15 @@
 import { AnglePickerControl } from "@wordpress/components";
+import { useState } from "react";
 
 
 function WPAnglePickerControl(){
+    const [number, onNumberChange] = useState( 180 )
     return (
         <div className="wp-component-wrapper bg">
-            <AnglePickerControl />
+            <AnglePickerControl 
+                value={ number }
+                onChange={ onNumberChange }
+            />
         </div>
     )
 }

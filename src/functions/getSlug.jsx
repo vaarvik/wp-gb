575e1ade75d0e1e472de7095f4c573d67c4d29c0
johann.varvik@drift.oslo.kommune.no <johann.varvik@drift.oslo.kommune.no>
2022-09-27 23:33:18 +0200

    Remove dash for space in slug to match previous permalinks

--- src/functions/getSlug.jsx
+++ src/functions/getSlug.jsx
@@ -3,7 +3,8 @@ const getSlug = (slug) => {
         .toLowerCase()
         .trim()
         .replace(/[^\w\s-]/g, '')
-        .replace(/[\s_-]+/g, '-')
+        // .replace(/[\s_-]+/g, '-')
+        .replace(/[\s_-]+/g, '')
         .replace(/^-+|-+$/g, '');
 }
 

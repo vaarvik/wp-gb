f8bd8ad20f2fed450b140d8f4cd5096fcf8ca880
johann.varvik@drift.oslo.kommune.no <johann.varvik@drift.oslo.kommune.no>
2022-09-29 00:18:16 +0200

    Add SearchBar and search functionality

--- src/components/BlockList/BlockList.jsx
+++ src/components/BlockList/BlockList.jsx
@@ -1,18 +1,19 @@
-import components from "../gutenberg/components";
 import BlockItem from "./BlockItem";
+import useGutenbergStore from "../../store/store"
 
 function BlockList() {
+  const components = useGutenbergStore(state => state.components);
   return (
     <div className="BlockList" id="BlockList">
       <ul className="grid cols-3">
         {
-          components.map(component => (
-            <li className="grid__item">
+          components.map((component, index) => (
+            <li className="grid__item" key={index}>
               <BlockItem
                 component={component}
                 category="components"
               >
-              { component.render() }
+              <component.render/>
             </BlockItem>
           </li>
           ))

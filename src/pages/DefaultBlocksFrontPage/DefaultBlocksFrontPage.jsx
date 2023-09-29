5032a96c25503b7b80fe5ba54987e292f7b73ec4
Sérgio Santos <me@s3rgiosan.com>
2023-09-30 00:54:21 +0100

    Fix WordPress typo

--- src/pages/DefaultBlocksFrontPage/DefaultBlocksFrontPage.jsx
+++ src/pages/DefaultBlocksFrontPage/DefaultBlocksFrontPage.jsx
@@ -9,7 +9,7 @@ function DefaultBlocksFrontPage() {
 	return (
 		<div className="FrontPage">
 			<PageHeader
-				title="Wordpress Gutenberg"
+				title="WordPress Gutenberg"
 				preamble="Create WordPress editor blocks using Gutenberg components! This site provides snippets, images and information about all tools available when developing rich WP blocks."
 			>
 				<p>This site is an open source project. Please join the community and add functionalities, components or requests <a href="https://github.com/vaarvik/wp-gb" target="_blank" rel="noreferrer">here</a>.</p>
